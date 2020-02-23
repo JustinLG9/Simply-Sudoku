901c0fc6c56ee2d957faa79bdab579de7ee03604
Justin Gargano <jlgargan@ncsu.edu>
2020-02-23 01:51:02 -0500

    test run 2

--- app.js
+++ app.js
@@ -22,7 +22,11 @@ app.use(function(req, res, next) {
   res.render('404');
 });
 
-
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
 
 //-------------------------- Routing --------------------------//
 app.get('/', function(req, res, next) {
